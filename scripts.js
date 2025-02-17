1a0d88a739a29672fab0addd18aea10ccce953bb
rodrigo290788 <rodrigo.alarcon.municipio.perico@gmail.com>
2025-02-17 12:23:27 -0300

    modificación ingreso de datos

--- scripts.js
+++ scripts.js
@@ -68,12 +68,29 @@ async function loadFiles(searchTerm = "") {
       if (renderedFiles.has(doc.id)) return;
       const data = doc.data();
       if (!searchTerm || Object.values(data).some((value) => value.toString().toLowerCase().includes(searchTerm.toLowerCase()))) {
-        const formattedDate = new Date(data.date).toLocaleDateString("es-ES", { day: "2-digit", month: "2-digit", year: "numeric" });
+        let formattedEntryDate = new Date(data.entryDate).toLocaleDateString("es-ES", { day: "2-digit", month: "2-digit", year: "numeric" });
+        if (formattedEntryDate == "Invalid Date") {
+          formattedEntryDate = "";
+        }
+
+        let formattedApprovalDate = new Date(data.approvalDate).toLocaleDateString("es-ES", { day: "2-digit", month: "2-digit", year: "numeric" });
+        if (formattedApprovalDate == "Invalid Date") {
+          formattedApprovalDate = "";
+        }
+        
         const li = document.createElement("li");
         li.className = "list-group-item d-flex justify-content-between align-items-center";
         li.innerHTML = `
-          <div>
-            <strong>${data.projectName.toUpperCase()}</strong> (${data.projectType.toUpperCase()}) -<strong> Estado</strong>: ${data.projectStatus} - <strong>Comisión</strong>: ${data.commission || "N/A"} - <strong>Ordenanza</strong>: ${data.ordinance} - <strong>Expediente</strong>: ${data.expedient} - <strong>Fecha</strong>: ${formattedDate} - <a href="${data.fileLink}" target="_blank">Ver archivo</a>
+         <div>
+            ${data.projectName ? `<strong>${data.projectName.toUpperCase()}</strong>` : ""}
+            ${data.projectType ? `(${data.projectType.toUpperCase()})` : ""}
+            ${data.projectStatus ? `- <strong>Estado:</strong> ${data.projectStatus}` : ""}
+            ${data.commission ? `- <strong>Comisión:</strong> ${data.commission}` : ""}
+            ${data.ordinance ? `- <strong>Ordenanza:</strong> ${data.ordinance}` : ""}
+            ${data.expedient ? `- <strong>Expediente:</strong> ${data.expedient}` : ""}
+            ${formattedEntryDate ? `- <strong>Fecha de Ingreso:</strong> ${formattedEntryDate}` : ""}
+            ${formattedApprovalDate ? `- <strong>Fecha de Aprobación:</strong> ${formattedApprovalDate}` : ""}
+            ${data.fileLink ? `- <a href="${data.fileLink}" target="_blank">Ver archivo</a>` : ""}
           </div>
           ${auth.currentUser ? `
           <div>
@@ -95,6 +112,9 @@ document.getElementById("search-query").addEventListener("input", (event) => {
 });
 
 window.uploadFile = async function () {
+  const saveButton = document.getElementById("save-btn");
+  saveButton.disabled = true;
+
   const id = document.getElementById("file-id").value;
   const data = {
     projectName: document.getElementById("project-name").value,
@@ -103,7 +123,8 @@ window.uploadFile = async function () {
     commission: document.getElementById("commission").value,
     ordinance: document.getElementById("ordinance").value,
     expedient: document.getElementById("expedient").value,
-    date: document.getElementById("project-date").value,
+    entryDate: document.getElementById("project-entry-date").value || "",
+    approvalDate: document.getElementById("approval-date").value || "",
     fileLink: document.getElementById("file-link").value,
   };
   try {
@@ -119,6 +140,35 @@ window.uploadFile = async function () {
     loadFiles();
   } catch (error) {
     alert("Error al guardar el archivo: " + error.message);
+  } finally {
+    saveButton.disabled = false;
+  }
+};
+
+window.editFile = async function (id) {
+  try {
+    const fileDoc = await getDoc(doc(db, "files", id));
+    const data = fileDoc.data();
+    document.getElementById("file-id").value = id;
+    document.getElementById("project-name").value = data.projectName;
+    document.getElementById("project-type").value = data.projectType;
+    document.getElementById("ordinance").value = data.ordinance;
+    document.getElementById("expedient").value = data.expedient;
+    document.getElementById("project-entry-date").value = data.entryDate;
+    document.getElementById("approval-date").value = data.approvalDate;
+    document.getElementById("file-link").value = data.fileLink;
+  } catch (error) {
+    alert("Error al cargar los datos para edición: " + error.message);
+  }
+};
+
+window.deleteFile = async function (id) {
+  try {
+    await deleteDoc(doc(db, "files", id));
+    alert("Archivo eliminado con éxito.");
+    loadFiles();
+  } catch (error) {
+    alert("Error al eliminar el archivo: " + error.message);
   }
 };
 
